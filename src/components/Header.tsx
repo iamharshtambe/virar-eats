d4205aa63637240d7b252165a0cb3662d3f090f8
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-16 22:05:12 +0530

    add header and search component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,19 @@
+import SearchBar from './SearchBar.tsx';
+
 function Header() {
    return (
-      <div className="flex items-center justify-between my-8">
-         <h1 className="font-mysoul text-5xl cursor-pointer">Virar Eats</h1>
-         <div className="flex gap-20 text-lg">
-            <a>Home</a>
-            <a>About Us</a>
-            <a>Cart</a>
+      <>
+         <div className="flex items-center justify-between my-8">
+            <h1 className="font-mysoul text-5xl cursor-pointer">Virar Eats</h1>
+            <div className="flex gap-20 text-lg">
+               <a className="cursor-pointer">Home</a>
+               <a className="cursor-pointer">About Us</a>
+               <a className="cursor-pointer">Cart</a>
+            </div>
          </div>
-      </div>
+
+         <SearchBar />
+      </>
    );
 }
 
