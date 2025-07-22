edcf8baee86bbb47957e71a3f5dbba2e47cd4238
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-22 13:36:26 +0530

    add custom hook to check online or offline status

---
+++ src/hooks/useStatus.ts
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export function useStatus() {
+   const [status, setStatus] = useState(true);
+
+   window.addEventListener('online', () => setStatus(true));
+
+   window.addEventListener('offline', () => setStatus(false));
+
+   return status;
+}
